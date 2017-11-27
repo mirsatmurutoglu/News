31cb09dc030bdba2dacfbe39fb847113bf11f38c
Mirsat <mirsat@mirsats-MacBook-Pro.local>
2017-11-27 20:58:20 +0800

    add SwiftyJSON pod, group file types

---
+++ News/Supporting Files/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  News
+//
+//  Created by mirsat on 27/11/17.
+//  Copyright © 2017 mirsat. All rights reserved.
+//
+
+import Foundation
+
+enum ApiDefaults {
+    static let baseUrl = "https://newsapi.org/v2/"
+    static let apiKey = "eb69a065eac445dc92e2e79b6147bcf0"
+}
