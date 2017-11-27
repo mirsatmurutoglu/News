7bcc507c7bb0a4cc0e615bec43bda6d324921f39
Mirsat <mirsat@mirsats-macbook-pro.local>
2017-11-27 19:48:13 +0800

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by mirsat on 27/11/17.
+//  Copyright © 2017 mirsat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
